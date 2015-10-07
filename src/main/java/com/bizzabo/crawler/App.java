cae8bfcdabe071d5c2937be4d189f8d511d1460a
chen <chen@web-pick.com>
2015-10-07 03:32:56 +0300

    ..

--- src/main/java/com/bizzabo/crawler/App.java
+++ src/main/java/com/bizzabo/crawler/App.java
@@ -1,23 +1,32 @@
 package com.bizzabo.crawler;
 
-
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 
-import java.io.IOException;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-/**
- * Hello world!
- *
- */
+
 public class App
 {
+    private static Set<String> pagesVisited = new HashSet<String>();
     private static int totalPagesToScan;
+
+    public static Set<String> getPagesVisited()
+    {
+        return pagesVisited;
+    }
+
+    public static void addPageToPagesVisited(String page)
+    {
+        App.pagesVisited.add(page);
+    }
 
     public static synchronized int getTotalPagesToScan()
     {
@@ -34,7 +43,7 @@ public class App
 
         Scanner in = new Scanner(System.in);
         System.out.println("Enter A Base Page Url");
-        String basePageUrl = in.nextLine();
+        String basePageUrl = in.nextLine().toLowerCase();
 
         if (!basePageUrl.startsWith("http://")){
             throw new RuntimeException("An Url Must Start With:  http:// format");
@@ -70,26 +79,50 @@ class Worker extends Thread {
     public void run() {
         try {
             while (true) {
-                String link = q.take();
+                String link = nextUrl();
                 if (link == null || App.getTotalPagesToScan() <=0) {
                     throw new InterruptedException();
                 }
+
+                Connection connection = Jsoup.connect(link);
+                Document htmlDocument = null;
+                try
+                {
+                    htmlDocument = connection.get();
+                } catch(Exception e)
+                {
+                    e.getStackTrace();
+                    break;
+                }
+                String regex = "http://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
+                Pattern p = Pattern.compile(regex);
+                Matcher m = p.matcher(htmlDocument.toString());
+                while (m.find()) {
+                    q.add(m.group());
+                }
+
                 System.out.println("Crawling Link: " + link);
                 System.out.println("Worker: " + currentThread().getName());
-                Connection connection = Jsoup.connect(link);
-                Document htmlDocument = connection.get();
-                Elements linksOnPage = htmlDocument.select("a[href]");
-                System.out.printf(linksOnPage.toString());
                 System.out.println("Page title: " + htmlDocument.select("title").text());
                 System.out.println('\n');
-                q.add("http://msn.co.il");
                 App.decTotalPagesToScan();
             }
         } catch (InterruptedException e) {
+            e.getStackTrace();
             System.exit(0);
-        } catch(IOException e)
+        }
+    }
+
+    public String nextUrl() throws InterruptedException
+    {
+        String link = q.take();
+        if (App.getPagesVisited().contains(link))
         {
-            e.printStackTrace();
+            return null;
+        }
+        else{
+            App.addPageToPagesVisited(link);
+            return link;
         }
     }
 }
